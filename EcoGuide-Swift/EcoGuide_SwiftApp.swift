913bbb19f3882dfaab7f1782313942c8f5b7677d
Farah_Torkhani <79060004+Farah-Torkhani@users.noreply.github.com>
2023-11-06 19:58:54 +0100

    Project Structure

---
+++ EcoGuide-Swift/EcoGuide_SwiftApp.swift
@@ -0,0 +1,20 @@
+//
+//  EcoGuide_SwiftApp.swift
+//  EcoGuide-Swift
+//
+//  Created by Torkhani fara on 6/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct EcoGuide_SwiftApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
