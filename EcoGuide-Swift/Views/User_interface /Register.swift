769a284a1080a3fa08e47269730f0c0b498964e5
CazadorBR <fedi.benromdhane@esprit.tn>
2023-11-07 17:34:16 +0100

    user interfaces

---
+++ EcoGuide-Swift/Views/User_interface /Register.swift
@@ -0,0 +1,20 @@
+//
+//  Register.swift
+//  EcoGuide-Swift
+//
+//  Created by ben romdhane fedi on 2023-11-07.
+//
+
+import SwiftUI
+
+struct Register: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Register_Previews: PreviewProvider {
+    static var previews: some View {
+        Register()
+    }
+}
---
+++ EcoGuide-Swift/Views/User_interface /Welcome_User.swift
@@ -0,0 +1,20 @@
+//
+//  Welcome_User.swift
+//  EcoGuide-Swift
+//
+//  Created by ben romdhane fedi on 2023-11-07.
+//
+
+import SwiftUI
+
+struct Welcome_User: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Welcome_User_Previews: PreviewProvider {
+    static var previews: some View {
+        Welcome_User()
+    }
+}
