0f97c12a27edf11b4925e0aafafe6cba2a0defc8
B4RB4RO554 <125827739+B4RB4RO554@users.noreply.github.com>
2023-11-09 01:35:28 +0100

    main interface

---
+++ EcoGuide-Swift/Views/Activities/MainPageActivities.swift
@@ -0,0 +1,217 @@
+//
+//  MainPageActivities.swift
+//  EcoGuide-Swift
+//
+//  Created by Copper_Orka on 8/11/2023.
+//
+
+import SwiftUI
+
+struct MainPageActivities: View {
+    @State private var username: String = ""
+    var image = ["cappadocia", "safari", "art", "fish"]
+    @State private var imageInfoList: [ImageInfo] = [
+        ImageInfo(imageName: "cappadocia", title: "Ballooning", location: "Europe, Turkey", price: "Cappadocia", rating: "String"),
+        ImageInfo(imageName: "safari", title: "Safari", location: "Africa, Kenya", price: "Massai Mara", rating: "string"),
+        ImageInfo(imageName: "art", title: "Art Exhebition", location: "Europe, France", price: "Louvre Paris", rating: "string"),
+        ImageInfo(imageName: "fish", title: "Scubadiving", location: "Oceania, Australia", price: "Coral Reef", rating: "string"),// Add more image info items for each image// Add more image info items for each image
+     ]
+    
+    var imagecat = ["wildlife", "marine", "artt", "artt","taj","sal"]
+    @State private var imageInfoCat: [ImageInfos] = [
+        ImageInfos(imageName: "wildlife",  title: "Wild Life", location: "Europe, Turkey", price: "Cappadocia", rating: "String"),
+        ImageInfos(imageName: "marine", title: "Marine Life", location: "Europe, Turkey", price: "Cappadocia", rating: "String"),
+        ImageInfos(imageName: "adrenaline", title: "Adrenaline", location: "Europe, Turkey", price: "Cappadocia", rating: "String"),
+        ImageInfos(imageName: "artt", title: "Art Venues", location: "Europe, Turkey", price: "Cappadocia", rating: "String"),
+        ImageInfos(imageName: "taj", title: "Sightseeing", location: "Europe, Turkey", price: "Cappadocia", rating: "String"),
+        ImageInfos(imageName: "sal", title: "Gastronomy", location: "Europe, Turkey", price: "Cappadocia", rating: "String"),// Add more image info items for each image// Add more image info items for each image
+     ]
+    
+    @State private var selectedFilter = "Recommended"
+
+        let filters = ["Recommended", "Popular", "Trending", "Testing"]
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                ZStack {
+                    VStack(alignment: .leading, spacing: 20)
+                    {
+                        Text("Hello, User")
+                        TextField(
+                            "Search",
+                            text: $username
+                        )
+                        .padding(.trailing, 20)
+                        .textFieldStyle(CustomTextFieldStyle())
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack {
+                                ForEach(filters, id: \.self) { filter in
+                                    Button(action: {
+                                        selectedFilter = filter
+                                    }) {
+                                        Text(filter)
+                                            .foregroundColor(selectedFilter == filter ? .white : .blue)
+                                            .padding(EdgeInsets(top: 12, leading: 25, bottom: 12, trailing: 25))
+                                            .background(selectedFilter == filter ? Color.blue : Color.clear)
+                                            .cornerRadius(32)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 32)
+                                                    .stroke(selectedFilter == filter ? Color.blue : Color.blue, lineWidth: 2)
+                                            )
+                                    }
+                                }
+                            }
+                        }
+                        
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 20) {
+                                ForEach(imageInfoList) { imageInfo in
+                                    ZStack {
+                                        Image(imageInfo.imageName)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 300, height: 380)
+                                            .cornerRadius(25)
+                                        HStack {
+                                            VStack(alignment: .leading, spacing: 10) {
+                                                Text(imageInfo.title)
+                                                    .font(.headline)
+                                                    .foregroundColor(.white)
+                                                Text(imageInfo.location)
+                                                    .font(.headline)
+                                                    .foregroundColor(.white)
+                                                Text(imageInfo.price)
+                                                    .font(.headline)
+                                                    .foregroundColor(.white)
+                                            }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+                                            .padding()
+                                            Spacer()
+                                            Text("Rating")
+                                                .padding()
+                                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                                        }
+                                    
+                                    }
+                                    
+                                    .frame(width: 300, height: 380)
+                                    
+                                }
+                            }
+                        }
+                        
+                        HStack {
+                            Text("Categories")
+                                .font(.system(size: 28, weight: .bold))
+                        }.padding(.trailing, 20)
+                        
+                        
+                        
+                        
+                        
+                      
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 20) {
+                                ForEach(imageInfoCat) { imageInfos in
+                                    ZStack {
+                                        Image(imageInfos.imageName)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 300, height: 180)
+                                            .cornerRadius(25)
+                                        
+                                   
+                                        Text(imageInfos.title)
+                                        
+                                            .font(.title)
+                                            .multilineTextAlignment(.center)
+                                            .foregroundColor(.white)
+                                            
+                                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                                            .padding()
+                                
+                                    
+                                    }
+                                    
+                                    .frame(width: 300, height: 180)
+                                    
+                                }
+                            }
+                        }
+                        
+                        
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            // Logique de connexion
+         
+                        }) {
+                            Text("Plan a Trip")
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                                .padding()
+                                .background(Color.blue)
+                                .cornerRadius(20)
+                                .padding(.horizontal, 32)
+                        }
+                        .padding(.top, 20)
+                       // .shadow(color: .black, radius: 70, x: 1, y: 30)
+                        
+                        
+                        Button(action: {
+                            // Logique de connexion
+         
+                        }) {
+                            Text("Plan a Trip")
+                                .fontWeight(.bold)
+                                .foregroundColor(.blue)
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                                .padding()
+                                .background(Color.init(hex:"F1F6FE"))
+                                .cornerRadius(20)
+                                .padding(.horizontal, 32)
+                        }
+                        .shadow(color: .black, radius: 70, x: 1, y: 30)
+                        
+                        
+                    }.padding(.leading, 20)
+                    
+                }
+            }.toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    Button (action: {})
+                    {
+                        HStack {
+                            //Text("Good Morning, \(Auth.currentUser?.firstname ?? "")")
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                        }
+                    }
+                    .foregroundColor(Color(.black))
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button (action: {
+                        //
+                    })
+                    {
+                        HStack {
+                            Image(systemName: "bookmark")
+                        }
+                    }
+                    .foregroundColor(Color(.blue))
+                }
+            }.padding(.top, 20)
+            
+        }
+    }
+}
+
+struct MainPageActivities_Previews: PreviewProvider {
+    static var previews: some View {
+        MainPageActivities()
+    }
+}
--- EcoGuide-Swift/Views/Activities/WelcomeMsgActivities.swift
+++ EcoGuide-Swift/Views/Activities/WelcomeMsgActivities.swift
@@ -12,162 +12,34 @@ import SwiftUI
 
 
 struct WelcomeMsgActivities: View {
-    @State private var username: String = ""
-    var image = ["guide1", "guide2", "guide3"]
-    @State private var imageInfoList: [ImageInfo] = [
-        ImageInfo(imageName: "guide1", title: "Flen Fouleni", location: "tunis, France", price: "$29 / day", rating: "String"),
-        ImageInfo(imageName: "guide2", title: "Flen Fouleni", location: "Paris, France", price: "$29 / day", rating: "string"),
-        ImageInfo(imageName: "guide3", title: "Flen Fouleni", location: "Paris, France", price: "$29 / day", rating: "string"),
-        ImageInfo(imageName: "guide3", title: "Flen Fouleni", location: "Paris, France", price: "$29 / day", rating: "string"),// Add more image info items for each image// Add more image info items for each image
-     ]
-    
-    
-    @State private var selectedFilter = "Recommended"
-
-        let filters = ["Recommended", "Popular", "Trending", "Testing"]
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                ZStack {
-                    VStack(alignment: .leading, spacing: 20)
-                    {
-                        Text("Hello, User")
-                        TextField(
-                            "Search",
-                            text: $username
-                        )
-                        .padding(.trailing, 20)
-                        .textFieldStyle(CustomTextFieldStyle())
-                        
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack {
-                                ForEach(filters, id: \.self) { filter in
-                                    Button(action: {
-                                        selectedFilter = filter
-                                    }) {
-                                        Text(filter)
-                                            .foregroundColor(selectedFilter == filter ? .white : .blue)
-                                            .padding(EdgeInsets(top: 12, leading: 25, bottom: 12, trailing: 25))
-                                            .background(selectedFilter == filter ? Color.blue : Color.clear)
-                                            .cornerRadius(32)
-                                            .overlay(
-                                                RoundedRectangle(cornerRadius: 30)
-                                                    .stroke(selectedFilter == filter ? Color.blue : Color.blue, lineWidth: 2)
-                                            )
-                                    }
-                                }
-                            }
-                        }
-                        
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack(spacing: 10) {
-                                ForEach(imageInfoList) { imageInfo in
-                                    ZStack {
-                                        Image(imageInfo.imageName)
-                                            .resizable()
-                                            .scaledToFill()
-                                            .frame(width: 300, height: 300)
-                                            .cornerRadius(50)
-                                        HStack {
-                                            VStack(alignment: .leading, spacing: 10) {
-                                                Text(imageInfo.title)
-                                                    .font(.headline)
-                                                    .foregroundColor(.white)
-                                                Text(imageInfo.location)
-                                                    .font(.headline)
-                                                    .foregroundColor(.black)
-                                                Text(imageInfo.price)
-                                                    .font(.headline)
-                                                    .foregroundColor(.white)
-                                            }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
-                                            .padding()
-                                            Spacer()
-                                            Text("Rating")
-                                                .padding()
-                                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
-                                        }
-                                    
-                                    }
-                                    
-                                    .frame(width: 300, height: 300)
-                                    
-                                }
-                            }
-                        }
-                        
-                        HStack {
-                            Text("Recently Booked")
-                                .font(.system(size: 22, weight: .semibold))
-                            Spacer()
-                            Text("See all")
-                                .font(.system(size: 22, weight: .semibold))
-                                .foregroundColor(Color.blue)
-                        }.padding(.trailing, 20)
-                        
-                        ForEach(imageInfoList) { imageInfo in
-                            ZStack {
-                                Color(hex: "F3F8FE") // Set your desired background color here
-                                    .frame(width: 350, height: 150)
-                                    .cornerRadius(20)
-                                HStack(spacing: 5) {
-                                    Image(imageInfo.imageName)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 100, height: 100)
-                                    VStack(alignment: .leading, spacing: 15) {
-                                        Text(imageInfo.imageName)
-                                            .font(.system(size: 23, weight: .semibold))
-                                        Text(imageInfo.location)
-                                            .font(.system(size: 16))
-                                            .foregroundColor(Color.gray)
-                                        Text(imageInfo.rating)
-                                            .font(.system(size: 15))
-                                    }
-                                    .frame(maxWidth: .infinity) // Expand to fill the available space
-                                    Spacer()
-                                    VStack(alignment: .trailing, spacing: 15) {
-                                        Text("$35")
-                                            .font(.system(size: 23, weight: .semibold))
-                                            .foregroundColor(Color.blue)
-                                        Text("/ day")
-                                        Text("Button")
-                                        
-                                    }
-                                }
-                                .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
-                            }
-                            
-                        }.padding(.trailing, 20)
-                        
-                    }.padding(.leading, 20)
-                    
-                }
-            }.toolbar {
-                ToolbarItemGroup(placement: .navigationBarLeading) {
-                    Button (action: {})
-                    {
-                        HStack {
-                            //Text("Good Morning, \(Auth.currentUser?.firstname ?? "")")
-                            Image("logo")
-                                .resizable()
-                                .scaledToFit()
-                        }
-                    }
-                    .foregroundColor(Color(.black))
-                }
-                ToolbarItemGroup(placement: .navigationBarTrailing) {
-                    Button (action: {
-                        //
-                    })
-                    {
-                        HStack {
-                            Image(systemName: "bell")
-                        }
-                    }
-                    .foregroundColor(Color(.black))
-                }
-            }.padding(.top, 20)
+        ZStack{
+            Image("man")
+                .resizable()
+                .frame(width: 600, height: 900)
+                .clipped()
+                .edgesIgnoringSafeArea(.all)
             
+            ZStack{
+               Image("white")
+                    .opacity(0.9)
+                   
+                
+                
+            }
+            VStack{
+                Spacer()
+                ZStack{
+                    Text("hello")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(20)
+                        .padding(.horizontal, 32)
+                }
+            }
         }
     }
 }
